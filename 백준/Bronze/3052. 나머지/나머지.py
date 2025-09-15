3c4c74914e49f9c055d0db26eb8948d1e5ca6871
이효은 <s25049@gsm.hs.kr>
2025-09-15 20:45:11 +0900

    Update 나머지.py

--- 백준/Bronze/3052. 나머지/나머지.py
+++ 백준/Bronze/3052. 나머지/나머지.py
@@ -3,4 +3,9 @@ arr = []
 for i in range(10):
     num = int(input())
     arr.append(num % 42)
+
 print(len(set(arr)))
+
+# append : 리스트에 값 넣을 때 사용
+# len : 몇 개 들어있는지 개수 세는 함수
+# set : 리스트에서 중복 없애고, 몇 개인지 셀 때 자주 같이 사용
