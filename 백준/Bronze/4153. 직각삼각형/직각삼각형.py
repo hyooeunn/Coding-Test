99faf66e1e55343f71aa1ec9b894e60d160ed0b9
이효은 <s25049@gsm.hs.kr>
2025-09-17 14:01:37 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,10 @@
+while True:
+    t = list(map(int, input().split()))
+    if t[0] == 0 and t[1] == 0 and t[2] == 0:
+        break
+        
+    t.sort()
+    if t[0] ** 2 + t[1] ** 2 == t[2] ** 2:
+        print('right')
+    else:
+        print('wrong')
