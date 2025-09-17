dfceb1284ad6a7945cf7546f68dcff7a9c7be1f7
이효은 <s25049@gsm.hs.kr>
2025-09-17 20:44:10 +0900

    Update 삼각형의 완성조건 （1）.js

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,8 +1,11 @@
 function solution(sides) {
     let max = Math.max(...sides);
-    
+
+    // (a + b) => a + b : 누적값(a) + 현재값(b)을 계산 | 0은 초기값
     let sum =  sides.reduce((a, b) => a + b, 0 ) - max;
     
     if (max < sum) return 1;
     else return 2;
 }
+
+// reduce : 배열을 하나의 값으로 누적 계산할 때 사용
