a871b5b4af2b44061b1ba33dbcaf947be79ebfec
이효은 <s25049@gsm.hs.kr>
2025-09-17 14:04:47 +0900

    Update 직각삼각형.py

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.py
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -3,8 +3,11 @@ while True:
     if t[0] == 0 and t[1] == 0 and t[2] == 0:
         break
         
-    t.sort()
+    t.sort() # 제일 큰 변을 맨 뒤로 보내주는 것
     if t[0] ** 2 + t[1] ** 2 == t[2] ** 2:
         print('right')
     else:
+
         print('wrong')
+
+# sort() : 작은 것부터 큰 것 순서대로 정렬 
