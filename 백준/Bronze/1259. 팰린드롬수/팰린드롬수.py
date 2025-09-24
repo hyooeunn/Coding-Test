24e31090af4dca2cfec4537029c8a9a2a79633a0
이효은 <s25049@gsm.hs.kr>
2025-09-24 16:19:21 +0900

    Update 팰린드롬수.py

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.py
@@ -1,3 +1,4 @@
+# 슬라이싱
 while True:
     n = input().strip()
     
@@ -6,4 +7,8 @@ while True:
     elif n == n[::-1]:
         print('yes')
     else:
+
         print('no')
+
+# strip() : 양쪽 공백을 제거해주는 매서드
+# n[::-1] : n을 뒤집은 문자열을 의미한다.
