e495e953c1a6e038f95f0d670456f0f68373b158
이효은 <s25049@gsm.hs.kr>
2025-09-29 17:36:41 +0900

    Update 단어 정렬.py

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -10,4 +10,14 @@ words.sort()
 words.sort(key = len)
 
 for j in words:
+
     print(j)
+
+# 변수명.sort() : 안의 값들을 오름차순으로 정렬
+
+# key 매개 변수
+# - sort()에는 key라는 옵션 존재
+# key는 정렬 기준을 바꿔줌
+
+# len : 문자열의 길이 반환
+# *길이가 짧은 순서대로 정렬하려고 씀
