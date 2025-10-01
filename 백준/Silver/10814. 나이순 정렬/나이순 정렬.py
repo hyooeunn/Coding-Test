0611f521a4500731530257f8dea7ec9c402803a3
이효은 <s25049@gsm.hs.kr>
2025-10-01 13:44:18 +0900

    Update 나이순 정렬.py

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -8,4 +8,10 @@ for _ in range(n):
 li.sort(key = lambda x : int(x[0]))
 
 for i in li:
+
     print(i[0], i[1])
+
+# lambda : 함수 만드는 키워드 (이름 없는 간단한 함수)
+# x → 정렬할 때, li 안의 각 원소 [age, name]이 하나씩 들어옴
+# x[0] → 그 원소의 첫 번째 값(age)
+# int(x[0]) → 그 값을 정수로 변환
