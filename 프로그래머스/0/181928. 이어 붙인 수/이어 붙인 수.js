ba1b9df8c60caae122e7bedabc30cfa868fc9e9a
이효은 <s25049@gsm.hs.kr>
2025-10-22 14:17:36 +0900

    [level 0] Title: 이어 붙인 수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.js
@@ -0,0 +1,18 @@
+function solution(num_list) {
+    let answer = 0;
+    let odd = '';
+    let even = '';
+    
+    for (let i = 0; i < num_list.length; i++) {
+        if (num_list[i] % 2 != 0) {
+            odd += num_list[i].toString();
+        }
+        else {
+            even += num_list[i].toString();
+        }
+    }
+    
+    answer = parseInt(odd) + parseInt(even);
+    
+    return answer;
+}
