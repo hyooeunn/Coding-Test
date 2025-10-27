6c8da9b3b2284f9a4b9fc471916b1527a57827b2
이효은 <s25049@gsm.hs.kr>
2025-10-27 19:59:39 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,13 @@
+t = int(input())
+
+for _ in range(t):
+    k = int(input())
+    n = int(input())
+    
+    apt = [i for i in range(1, n + 1)]
+    
+    for i in range(k):
+        for j in range(1, n):
+            apt[j] += apt[j - 1]
+            
+    print(apt[n - 1])
